18f65f8c19dd60c9f5dff314880c13f1245cd55a
Lena Lomakina <lomakina.june1977@gmail.com>
2017-12-12 18:01:43 +0200

    first commit

---
+++ task2.js
@@ -0,0 +1,8 @@
+var input = prompt('Enter several numbers separated by commas');
+var arr = input.split(',');
+document.write("array: " + arr + "<br/>");
+var n = arr.length;
+// document.write("new array: ");
+for (var i=0; i<n; arr[i]++, i++);
+document.write("new array: " + arr);
+
